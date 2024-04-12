7ea62ef7a8da0e6c63059a4167ec8aa8ac2eaa2d
Carles Serna <carles.serna@gmail.com>
2024-04-12 12:35:42 +0200

    hero fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,22 @@
+import CarouselComponent from "./Carousel";
+
 function Hero() {
   return (
     <>
-      <section className="flex grid-cols-2 gap-1">
+      <section className="grid grid-cols-2 gap-4 px-8">
         <div className="info">
           <h1 className="title text-2xl font-bold">
             Su Proveedor de Herramientas y Recambios para Automoción
           </h1>
-          <div className="text">
+          <p className="text">
             Joma se especializa en la importación y distribución de herramientas
             y recambios para el sector automotriz. Ofrecemos productos de
             calidad a precios competitivos, ideales para distribuidores que
             abastecen directamente a talleres mecánicos.
-          </div>
+          </p>
         </div>
         <div className="carrousel">Carrousel</div>
+        <CarouselComponent />
       </section>
     </>
   );
