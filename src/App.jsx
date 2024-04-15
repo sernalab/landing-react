e7d9113f480e9320fb9a1feb99d2d3b85c61f686
Carles Serna <carles.serna@gmail.com>
2024-04-15 17:30:25 +0200

    component features

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Features from "./components/Features";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <main>
         <Hero />
+        <Features />
         <section>3 bloques: - Más de X productos - Envío en 48h -</section>
       </main>
     </>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,20 +3,26 @@ import CarouselComponent from "./Carousel";
 function Hero() {
   return (
     <>
-      <section className="grid grid-cols-2 gap-4 px-8">
-        <div className="info">
-          <h1 className="title text-2xl font-bold">
-            Su Proveedor de Herramientas y Recambios para Automoción
-          </h1>
-          <p className="text">
-            Joma se especializa en la importación y distribución de herramientas
-            y recambios para el sector automotriz. Ofrecemos productos de
-            calidad a precios competitivos, ideales para distribuidores que
-            abastecen directamente a talleres mecánicos.
-          </p>
+      <section className="bg-gray-200 p-4 md:p-8 lg:p-12">
+        <div className="max-w-screen-xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="flex flex-col justify-center items-center md:items-start text-center md:text-left">
+            <h1 className="text-2xl md:text-3xl lg:text-4xl font-bold mb-3">
+              Su proveedor de herramientas y recambios para automoción
+            </h1>
+            <p className="">
+              Joma se especializa en la importación y distribución de
+              herramientas y recambios para el sector automotriz. Ofrecemos
+              productos de
+              <span className="font-bold"> calidad</span> a{" "}
+              <span className="font-bold">precios competitivos</span>, ideales
+              para distribuidores que abastecen directamente a talleres
+              mecánicos.
+            </p>
+          </div>
+          <div className="carrousel">
+            <CarouselComponent />
+          </div>
         </div>
-        <div className="carrousel">Carrousel</div>
-        <CarouselComponent />
       </section>
     </>
   );
