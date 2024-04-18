b0ed9602d922794490d7b7824e808bcd4de162c5
Carles Serna <carles.serna@gmail.com>
2024-04-18 19:38:00 +0200

    working with test data

---
+++ src/components/KitCard.jsx
@@ -0,0 +1,28 @@
+function KitCard() {
+  return (
+    <>
+      <div class="flex flex-col bg-white border shadow-sm rounded-xl">
+        <img
+          class="w-full h-auto rounded-t-xl"
+          src="https://images.unsplash.com/photo-1680868543815-b8666dba60f7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2532&q=80"
+          alt="Image Description"
+        />
+        <div class="p-4 md:p-5">
+          <h3 class="text-lg font-bold text-gray-800">Card title</h3>
+          <p class="mt-1 text-gray-500">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <a
+            class="mt-2 py-2 px-3 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+            href="#"
+          >
+            Go somewhere
+          </a>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default KitCard;
--- src/pages/CatalogoPage.jsx
+++ src/pages/CatalogoPage.jsx
@@ -1,9 +1,57 @@
-// para descargar aqui:
+import { useState } from "react";
 
-// https://www.joma-tools.com/CatalogoJoma.pdf
+import categoriesData from "../data/categories.json";
+import ProductCarouselComponent from "../components/ProductCarousel";
 
 function CatalogoPage() {
-  return <>PAGINA CATALOGO</>;
+  const [selectedValue, setSelectedValue] = useState("");
+
+  const handleSelectChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
+
+  return (
+    <>
+      <section className="max-w-screen-xl mx-auto p-4 md:p-8 lg:p-12 h-lvh">
+        <ProductCarouselComponent />
+        <div className="flex justify-between mt-10">
+          <div>
+            <label for="hs-select-label" className="block  font-bold mb-2">
+              Categorías
+            </label>
+            <select
+              id="hs-select-label"
+              className="py-3 px-4 pe-9 block  border-2 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none"
+              onChange={handleSelectChange}
+            >
+              <option value="">Selecciona</option>
+              <option value="kits">Kits distribuciones</option>
+              <option value="mecanica">Mecánica</option>
+              <option value="diagnosis">Diagnosis</option>
+              <option value="electricidad">Electricidad</option>
+              <option value="chapa-pintura">Chapa y pintura</option>
+            </select>
+          </div>
+
+          <p>
+            Descarga{" "}
+            <a
+              href="https://www.joma-tools.com/CatalogoJoma.pdf"
+              download="catalogojoma.pdf"
+              target="_blank"
+              className="font-bold"
+            >
+              aquí
+            </a>{" "}
+            el catálogo en formato PDF.
+          </p>
+        </div>
+        <div>
+          <p>{selectedValue}</p>
+        </div>
+      </section>
+    </>
+  );
 }
 
 export default CatalogoPage;
