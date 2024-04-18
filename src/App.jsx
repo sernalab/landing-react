d8605698dc6430968c2134912b1055355e41d542
Carles Serna <carles.serna@gmail.com>
2024-04-18 13:57:55 +0200

    routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,24 @@
 import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Products from "./components/Products";
 import Footer from "./components/Footer";
+import CatalogoPage from "./pages/CatalogoPage";
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <Hero />
-        <Features />
-        <Products />
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          <Route path="/features" element={<Features />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/catalog" element={<CatalogoPage />} />
+        </Routes>
         <Footer />
       </main>
     </>
