cd764313448e7152937a84e81716c754a9e9e632
Carles Serna <carles.serna@gmail.com>
2024-04-18 19:32:15 +0200

    add new carousel cateogry

---
+++ src/components/ProductCarousel.jsx
@@ -0,0 +1,82 @@
+function ProductCarouselComponent() {
+  return (
+    <>
+      <div
+        data-hs-carousel='{
+    "loadingClasses": "opacity-0",
+    "isAutoPlay": true
+  }'
+        className="relative"
+      >
+        <div className="hs-carousel relative overflow-hidden w-full min-h-[350px] bg-white rounded-lg">
+          <div className="hs-carousel-body absolute top-0 bottom-0 start-0 flex flex-nowrap transition-transform duration-700 opacity-0">
+            <div className="hs-carousel-slide">
+              <div className="flex justify-center bg-gray-100 p-6">
+                <span className="self-center text-4xl transition duration-700">
+                  <img
+                    src="https://www.joma-tools.com/catalogo/modules/ps_imageslider/images/a2616d73ad1bf649d83290be50e677b9f2a65585_Banner_1.jpg"
+                    alt=""
+                  />
+                </span>
+              </div>
+            </div>
+            <div className="hs-carousel-slide">
+              <div className="flex justify-center h-full bg-gray-200 p-6">
+                <span className="self-center text-4xl transition duration-700">
+                  <img
+                    src="https://www.joma-tools.com/catalogo/modules/ps_imageslider/images/e573ac5d69238edb669ae0e7231f4763cf710a46_Banner_2.jpg"
+                    alt=""
+                  />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <button
+          type="button"
+          className="hs-carousel-prev hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 start-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/[.1]"
+        >
+          <span className="text-2xl" aria-hidden="true">
+            <svg
+              className="flex-shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"></path>
+            </svg>
+          </span>
+          <span className="sr-only">Previous</span>
+        </button>
+        <button
+          type="button"
+          className="hs-carousel-next hs-carousel:disabled:opacity-50 disabled:pointer-events-none absolute inset-y-0 end-0 inline-flex justify-center items-center w-[46px] h-full text-gray-800 hover:bg-gray-800/[.1]"
+        >
+          <span className="sr-only">Next</span>
+          <span className="text-2xl" aria-hidden="true">
+            <svg
+              className="flex-shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+            </svg>
+          </span>
+        </button>
+
+        <div className="hs-carousel-pagination flex justify-center absolute bottom-3 start-0 end-0 space-x-2">
+          <span className="hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer"></span>
+          <span className="hs-carousel-active:bg-blue-700 hs-carousel-active:border-blue-700 size-3 border border-gray-400 rounded-full cursor-pointer"></span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductCarouselComponent;
