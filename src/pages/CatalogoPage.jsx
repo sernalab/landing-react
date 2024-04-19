b0b3d918f193fcb893f1030bbf8549937716c9c2
Carles Serna <carles.serna@gmail.com>
2024-04-19 10:57:02 +0200

    showing length cards

--- src/pages/CatalogoPage.jsx
+++ src/pages/CatalogoPage.jsx
@@ -2,17 +2,25 @@ import { useState } from "react";
 
 import categoriesData from "../data/categories.json";
 import ProductCarouselComponent from "../components/ProductCarousel";
+import KitCard from "../components/KitCard";
 
 function CatalogoPage() {
   const [selectedValue, setSelectedValue] = useState("");
 
   const handleSelectChange = (event) => {
+    console.log("Selected value: ", event.target.value);
     setSelectedValue(event.target.value);
   };
 
+  const selectedCategory = categoriesData.categories.find(
+    (category) => category.name === selectedValue
+  );
+
+  console.log("Selected category: ", selectedCategory);
+
   return (
     <>
-      <section className="max-w-screen-xl mx-auto p-4 md:p-8 lg:p-12 h-lvh">
+      <section className="max-w-screen-xl mx-auto p-4 md:p-8 lg:p-12">
         <ProductCarouselComponent />
         <div className="flex justify-between mt-10">
           <div>
@@ -46,8 +54,11 @@ function CatalogoPage() {
             el catálogo en formato PDF.
           </p>
         </div>
-        <div>
-          <p>{selectedValue}</p>
+        <div className="grid grid-cols-4 mt-10 gap-5">
+          {selectedCategory &&
+            selectedCategory.subcategory.map((subcat) => (
+              <KitCard key={subcat} name={subcat} />
+            ))}
         </div>
       </section>
     </>
