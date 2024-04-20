de1661bdae245e5b0c78a8bea970f083ce4498dd
Carles Serna <carles.serna@gmail.com>
2024-04-20 20:38:54 +0200

    products block done

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,96 +3,162 @@ import { Link } from "react-router-dom";
 function Products() {
   return (
     <>
-      <section class="max-w-screen-xl mx-auto p-4 md:p-8 lg:p-12">
-        <div class="grid sm:grid-cols-12 gap-3">
-          <div class="sm:self-end col-span-12 sm:col-span-7 md:col-span-8 lg:col-span-5 lg:col-start-3">
-            <a class="group relative block rounded-xl overflow-hidden" href="#">
-              <div class="aspect-w-12 aspect-h-7 sm:aspect-none rounded-xl overflow-hidden border-solid border-2 border-slate-300">
+      <section className="max-w-screen-xl mx-auto p-4 md:p-8 lg:p-12">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4">
+          <div className="md:col-span-2 lg:col-span-1">
+            <a
+              className="group relative block rounded-xl overflow-hidden"
+              href="#"
+            >
+              <div className="aspect-w-1 aspect-h-1 rounded-xl overflow-hidden border-solid border-2 border-slate-300">
                 <img
                   src="https://www.joma-tools.com/catalogo/837-large_default/lampara-cob-articulada-jolcoba.jpg"
                   alt=""
+                  className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500 ease-in-out"
                 />
               </div>
-              <div class="absolute bottom-0 start-0 end-0 p-2">
-                <div class="font-semibold text-gray-800 rounded-lg bg-gray-200 p-3">
+              <div className="absolute bottom-0 start-0 end-0 p-2 bg-gradient-to-t from-black to-transparent">
+                <div className="font-semibold text-white p-3">
                   Workplace personalities
                 </div>
               </div>
             </a>
           </div>
-
-          <div class="sm:self-end col-span-12 sm:col-span-5 md:col-span-4 lg:col-span-3">
-            <a class="group relative block rounded-xl overflow-hidden" href="#">
-              <div class="aspect-w-12 aspect-h-7 sm:aspect-none rounded-xl overflow-hidden border-solid border-2 border-slate-300">
+          <div className="md:col-span-2 lg:col-span-1">
+            <a
+              className="group relative block rounded-xl overflow-hidden"
+              href="#"
+            >
+              <div className="aspect-w-1 aspect-h-1 rounded-xl overflow-hidden border-solid border-2 border-slate-300">
                 <img
-                  class="group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl w-full object-cover"
                   src="https://www.joma-tools.com/catalogo/951-large_default/supresor-de-picos-12-y-24-voltios-josp.jpg"
                   alt="Image Description"
+                  className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500 ease-in-out"
                 />
               </div>
-              <div class="absolute bottom-0 start-0 end-0 p-2">
-                <div class="font-semibold text-gray-800 rounded-lg bg-gray-200 p-3">
-                  Engineering
-                </div>
+              <div className="absolute bottom-0 start-0 end-0 p-2 bg-gradient-to-t from-black to-transparent">
+                <div className="font-semibold text-white p-3">Engineering</div>
               </div>
             </a>
           </div>
-
-          <div class="col-span-12 md:col-span-4">
-            <a class="group relative block rounded-xl overflow-hidden" href="#">
-              <div class="aspect-w-12 aspect-h-7 sm:aspect-none rounded-xl overflow-hidden border-solid border-2 border-slate-300">
+          <div className="md:col-span-1 lg:col-span-1">
+            <a
+              className="group relative block rounded-xl overflow-hidden"
+              href="#"
+            >
+              <div className="aspect-w-1 aspect-h-1 rounded-xl overflow-hidden border-solid border-2 border-slate-300">
                 <img
-                  class="group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl w-full object-cover"
                   src="https://www.joma-tools.com/catalogo/791-large_default/kit-cableados-de-pruebas-jokit.jpg"
                   alt="Image Description"
+                  className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500 ease-in-out"
                 />
               </div>
-              <div class="absolute bottom-0 start-0 end-0 p-2">
-                <div class="font-semibold text-gray-800 rounded-lg bg-gray-200 p-3">
-                  Data 2021
-                </div>
+              <div className="absolute bottom-0 start-0 end-0 p-2 bg-gradient-to-t from-black to-transparent">
+                <div className="font-semibold text-white p-3">Data 2021</div>
               </div>
             </a>
           </div>
-
-          <div class="col-span-12 sm:col-span-6 md:col-span-4">
-            <a class="group relative block rounded-xl overflow-hidden" href="#">
-              <div class="aspect-w-12 aspect-h-7 sm:aspect-none rounded-xl overflow-hidden border-solid border-2 border-slate-300">
+          <div className="md:col-span-1 lg:col-span-1">
+            <a
+              className="group relative block rounded-xl overflow-hidden"
+              href="#"
+            >
+              <div className="aspect-w-1 aspect-h-1 rounded-xl overflow-hidden border-solid border-2 border-slate-300">
                 <img
-                  class="group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl w-full object-cover"
                   src="https://www.joma-tools.com/catalogo/896-large_default/polimetro-digital-rango-manual-vipd3000.jpg"
                   alt="Image Description"
+                  className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500 ease-in-out"
                 />
               </div>
-              <div class="absolute bottom-0 start-0 end-0 p-2">
-                <div class="font-semibold text-gray-800 rounded-lg bg-gray-200 p-3">
+              <div className="absolute bottom-0 start-0 end-0 p-2 bg-gradient-to-t from-black to-transparent">
+                <div className="font-semibold text-white p-3">
                   Data at Preline
                 </div>
               </div>
             </a>
           </div>
-
-          <div class="col-span-12 sm:col-span-6 md:col-span-4">
-            <a class="group relative block rounded-xl overflow-hidden" href="#">
-              <div class="aspect-w-12 aspect-h-7 sm:aspect-none rounded-xl overflow-hidden border-solid border-2 border-slate-300">
+          <div className="md:col-span-1 lg:col-span-1">
+            <a
+              className="group relative block rounded-xl overflow-hidden"
+              href="#"
+            >
+              <div className="aspect-w-1 aspect-h-1 rounded-xl overflow-hidden border-solid border-2 border-slate-300">
                 <img
-                  class="group-hover:scale-105 transition-transform duration-500 ease-in-out rounded-xl w-full object-cover"
-                  src="https://www.joma-tools.com/catalogo/915-large_default/termometros-por-infrarrojos-con-puntero-laser.jpg"
+                  src="https://www.joma-tools.com/catalogo/896-large_default/polimetro-digital-rango-manual-vipd3000.jpg"
                   alt="Image Description"
+                  className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500 ease-in-out"
                 />
               </div>
-              <div class="absolute bottom-0 start-0 end-0 p-2">
-                <div class="font-semibold text-gray-800 rounded-lg bg-gray-200 p-3">
-                  Empowered management
+              <div className="absolute bottom-0 start-0 end-0 p-2 bg-gradient-to-t from-black to-transparent">
+                <div className="font-semibold text-white p-3">
+                  Data at Preline
+                </div>
+              </div>
+            </a>
+          </div>
+          <div className="md:col-span-1 lg:col-span-1">
+            <a
+              className="group relative block rounded-xl overflow-hidden"
+              href="#"
+            >
+              <div className="aspect-w-1 aspect-h-1 rounded-xl overflow-hidden border-solid border-2 border-slate-300">
+                <img
+                  src="https://www.joma-tools.com/catalogo/896-large_default/polimetro-digital-rango-manual-vipd3000.jpg"
+                  alt="Image Description"
+                  className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500 ease-in-out"
+                />
+              </div>
+              <div className="absolute bottom-0 start-0 end-0 p-2 bg-gradient-to-t from-black to-transparent">
+                <div className="font-semibold text-white p-3">
+                  Data at Preline
+                </div>
+              </div>
+            </a>
+          </div>
+          <div className="md:col-span-1 lg:col-span-1">
+            <a
+              className="group relative block rounded-xl overflow-hidden"
+              href="#"
+            >
+              <div className="aspect-w-1 aspect-h-1 rounded-xl overflow-hidden border-solid border-2 border-slate-300">
+                <img
+                  src="https://www.joma-tools.com/catalogo/896-large_default/polimetro-digital-rango-manual-vipd3000.jpg"
+                  alt="Image Description"
+                  className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500 ease-in-out"
+                />
+              </div>
+              <div className="absolute bottom-0 start-0 end-0 p-2 bg-gradient-to-t from-black to-transparent">
+                <div className="font-semibold text-white p-3">
+                  Data at Preline
+                </div>
+              </div>
+            </a>
+          </div>
+          <div className="md:col-span-1 lg:col-span-1">
+            <a
+              className="group relative block rounded-xl overflow-hidden"
+              href="#"
+            >
+              <div className="aspect-w-1 aspect-h-1 rounded-xl overflow-hidden border-solid border-2 border-slate-300">
+                <img
+                  src="https://www.joma-tools.com/catalogo/896-large_default/polimetro-digital-rango-manual-vipd3000.jpg"
+                  alt="Image Description"
+                  className="w-full h-full object-cover group-hover:scale-110 transition-transform duration-500 ease-in-out"
+                />
+              </div>
+              <div className="absolute bottom-0 start-0 end-0 p-2 bg-gradient-to-t from-black to-transparent">
+                <div className="font-semibold text-white p-3">
+                  Data at Preline
                 </div>
               </div>
             </a>
           </div>
         </div>
+
         <div className="mt-12 flex justify-center">
           <Link
             className="bg-joma-color rounded-full p-5 text-white hover:text-white transition-transform duration-300 transform hover:scale-110"
-            to="/catalogo"
+            to="/catalog"
           >
             Más productos
           </Link>
