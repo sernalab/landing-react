3bbb86171572d756251e561529387a800202722a
Carles Serna <carles.serna@gmail.com>
2024-04-21 13:17:20 +0200

    flowbite added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,10 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "node_modules/preline/dist/*.js",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("preline/plugin")],
+  plugins: [require("preline/plugin"), require("flowbite/plugin")],
 };
