5934c7acc55caedc7bf693d543100befdb3f7b93
Carles Serna <carles.serna@gmail.com>
2024-04-22 17:43:35 +0200

    about page terminada

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,16 +5,16 @@ function AboutPage() {
         <div class="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
           <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
             <div class="max-w-lg">
-              <h2 class="text-3xl font-extrabold text-gray-900 sm:text-4xl">
-                About Us
+              <h2 class="text-xl font-extrabold text-gray-600 sm:text-4xl">
+                Tu proveedor de confianza en herramientas y recambios para
+                automoción
               </h2>
               <p class="mt-4 text-gray-600 text-lg">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                quis eros at lacus feugiat hendrerit sed ut tortor. Suspendisse
-                et magna quis elit efficitur consequat. Mauris eleifend velit a
-                pretium iaculis. Donec sagittis velit et magna euismod, vel
-                aliquet nulla malesuada. Nunc pharetra massa lectus, a fermentum
-                arcu volutpat vel.
+                En Joma, nos dedicamos a la importación y distribución de
+                herramientas, utillajes y recambios especializados para el
+                sector automotriz. Nuestra misión es ofrecerte productos de alta
+                calidad a precios competitivos, ideales para distribuidores que
+                proveen directamente a talleres mecánicos.
               </p>
               <div class="mt-8">
                 <a
@@ -34,7 +34,102 @@ function AboutPage() {
               />
             </div>
           </div>
+          <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-8 my-14">
+            <div class="mt-12 md:mt-0">
+              <img
+                src="https://images.unsplash.com/photo-1531973576160-7125cd663d86"
+                alt="About Us Image"
+                class="object-cover rounded-lg shadow-md"
+              />
+            </div>
+            <div class="max-w-lg">
+              <h2 class="text-xl font-extrabold text-gray-600 sm:text-4xl">
+                Comprometidos con la excelencia y la innovación
+              </h2>
+              <p class="mt-4 text-gray-600 text-lg">
+                Nos esforzamos por mantenerte al día con las últimas novedades
+                del mercado, asegurándonos de que siempre tengas acceso a lo
+                último en tecnología y eficiencia para el mantenimiento y
+                reparación de vehículos. Nuestra selección de productos está
+                cuidadosamente diseñada para cumplir con las demandas del sector
+                automotriz moderno.
+              </p>
+              <div class="mt-8">
+                <a
+                  href="#"
+                  class="text-blue-500 hover:text-blue-600 font-medium"
+                >
+                  Learn more about us
+                  <span class="ml-2">&#8594;</span>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
+            <div class="max-w-lg">
+              <h2 class="text-xl font-extrabold text-gray-600 sm:text-4xl">
+                Agilidad y flexibilidad en cada pedido
+              </h2>
+              <p class="mt-4 text-gray-600 text-lg">
+                Entendemos que el tiempo es crucial en tu negocio. Por eso,
+                nuestro eficiente sistema de gestión nos permite procesar y
+                enviar tu pedido en solo 24 horas, sin requerimiento de un
+                mínimo de compra. Esto te permite responder rápidamente a
+                cualquier necesidad urgente que pueda surgir en tu taller.
+              </p>
+              <div class="mt-8">
+                <a
+                  href="#"
+                  class="text-blue-500 hover:text-blue-600 font-medium"
+                >
+                  Learn more about us
+                  <span class="ml-2">&#8594;</span>
+                </a>
+              </div>
+            </div>
+            <div class="mt-12 md:mt-0">
+              <img
+                src="https://images.unsplash.com/photo-1531973576160-7125cd663d86"
+                alt="About Us Image"
+                class="object-cover rounded-lg shadow-md"
+              />
+            </div>
+          </div>
+          <div class="grid grid-cols-1 md:grid-cols-2 items-center gap-8 my-14">
+            <div class="mt-12 md:mt-0">
+              <img
+                src="https://images.unsplash.com/photo-1531973576160-7125cd663d86"
+                alt="About Us Image"
+                class="object-cover rounded-lg shadow-md"
+              />
+            </div>
+            <div class="max-w-lg">
+              <h2 class="text-xl font-extrabold text-gray-600 sm:text-4xl">
+                Estamos aquí para apoyarte
+              </h2>
+              <p class="mt-4 text-gray-600 text-lg">
+                Si eres un taller interesado en explorar nuestros productos, te
+                invitamos a enviarnos un correo electrónico con tus datos. Nos
+                encargaremos de ponerte en contacto con el distribuidor de tu
+                zona, garantizando un servicio personalizado y adaptado a tus
+                necesidades.
+              </p>
+              <div class="mt-8">
+                <a
+                  href="#"
+                  class="text-blue-500 hover:text-blue-600 font-medium"
+                >
+                  Learn more about us
+                  <span class="ml-2">&#8594;</span>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
+        <p>
+          En Joma, tu éxito es nuestro compromiso. Únete a nuestra red de
+          profesionales satisfechos y lleva tu negocio al siguiente nivel.
+        </p>
       </section>
     </>
   );
