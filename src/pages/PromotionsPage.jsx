a10ec15a0f7e79429cb229dc73b7a7e9be6767d0
Carles Serna <carles.serna@gmail.com>
2024-04-22 18:33:15 +0200

    promotionsPage with layout

---
+++ src/pages/PromotionsPage.jsx
@@ -0,0 +1,96 @@
+function Promotions() {
+  return (
+    <>
+      <section className="max-w-screen-xl mx-auto p-4 md:p-8 lg:p-12  py-12">
+        <div class="container mx-auto px-4">
+          <h2 class="text-3xl font-bold text-center mb-12 text-gray-600">
+            Actualmente en promoción:
+          </h2>
+          <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-6 gap-y-8">
+            <div class="group relative bg-gray-100 rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300">
+              <div class="absolute inset-0 bg-black bg-opacity-25 group-hover:bg-opacity-50 transition duration-300"></div>
+              <img
+                src="https://via.placeholder.com/500x300"
+                alt="Nombre del Producto"
+                class="w-full h-56 object-cover sm:h-72 md:h-96"
+              />
+              <div class="absolute bottom-0 left-0 p-4">
+                <h3 class="text-lg font-bold text-white">
+                  Nombre del Producto
+                </h3>
+                <p class="text-sm text-gray-300">Número de referencia</p>
+
+                <div class="mt-3 flex items-center space-x-4">
+                  <span class="text-xl font-semibold text-white">$89.99</span>
+                  <span class="text-base text-gray-400 line-through">
+                    $119.99
+                  </span>
+                </div>
+                <a
+                  href="#"
+                  class="inline-block mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300"
+                >
+                  Comprar Ahora
+                </a>
+              </div>
+            </div>
+            <div class="group relative bg-gray-100 rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300">
+              <div class="absolute inset-0 bg-black bg-opacity-25 group-hover:bg-opacity-50 transition duration-300"></div>
+              <img
+                src="https://via.placeholder.com/500x300"
+                alt="Nombre del Producto"
+                class="w-full h-56 object-cover sm:h-72 md:h-96"
+              />
+              <div class="absolute bottom-0 left-0 p-4">
+                <h3 class="text-lg font-bold text-white">
+                  Nombre del Producto
+                </h3>
+                <p class="text-sm text-gray-300">Número de referencia</p>
+                <div class="mt-3 flex items-center space-x-4">
+                  <span class="text-xl font-semibold text-white">$89.99</span>
+                  <span class="text-base text-gray-400 line-through">
+                    $119.99
+                  </span>
+                </div>
+                <a
+                  href="#"
+                  class="inline-block mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300"
+                >
+                  Comprar Ahora
+                </a>
+              </div>
+            </div>
+            <div class="group relative bg-gray-100 rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300">
+              <div class="absolute inset-0 bg-black bg-opacity-25 group-hover:bg-opacity-50 transition duration-300"></div>
+              <img
+                src="https://via.placeholder.com/500x300"
+                alt="Nombre del Producto"
+                class="w-full h-56 object-cover sm:h-72 md:h-96"
+              />
+              <div class="absolute bottom-0 left-0 p-4">
+                <h3 class="text-lg font-bold text-white">
+                  Nombre del Producto
+                </h3>
+                <p class="text-sm text-gray-300">Número de referencia</p>
+                <div class="mt-3 flex items-center space-x-4">
+                  <span class="text-xl font-semibold text-white">$89.99</span>
+                  <span class="text-base text-gray-400 line-through">
+                    $119.99
+                  </span>
+                </div>
+                <a
+                  href="#"
+                  class="inline-block mt-4 bg-blue-600 text-white px-4 py-2 rounded hover:bg-blue-700 transition duration-300"
+                >
+                  Comprar Ahora
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Promotions;
