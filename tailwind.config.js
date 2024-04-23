8cd0c676596640727368c1e718bb8c2804446c26
Carles Serna <carles.serna@gmail.com>
2024-04-23 12:20:47 +0200

    flowbite desinstalado

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,9 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "node_modules/preline/dist/*.js",
-    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("preline/plugin"), require("flowbite/plugin")],
+  plugins: [require("preline/plugin")],
 };
